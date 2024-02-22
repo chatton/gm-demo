6ec749efba11ba343478e8f9c0eb30f847ac9d5e
chatton <github.qpeyb@simplelogin.fr>
2024-02-22 13:09:23 +0000

    chore: adding python wasm script

--- gm/scripts/update-genesis.py
+++ gm/scripts/update-genesis.py
@@ -1,27 +1,28 @@
+import gzip
 import json
 import sys
-import gzip
+import base64
 
 
-def _read_wasm_bytes(wasm_contract_path):
-    # return wasm_contract_path
+def _read_wasm_bytes(wasm_contract_path: str):
     with open(wasm_contract_path, 'rb') as f:
-        return str(gzip.compress(f.read()))
+        gzipped = gzip.compress(f.read())
+        encoded = base64.b64encode(gzipped)
+        return encoded.decode('utf-8')
 
 
-def _modify_genesis(genesis_contents, genesis_file_path, wasm_contract_path):
+def _modify_genesis(genesis_contents: str, genesis_file_path: str, wasm_contract_path: str) -> None:
     data = genesis_contents
     if not data:
-        raise ValueError("failed to load genesis file")
+        raise ValueError("invalid genesis file contents")
 
     data = json.loads(data)
 
-    if '08-wasm' not in ['app_state']:
-        data['app_state']['08-wasm'] = {}
-
-    data['app_state']['08-wasm']['contracts'] = [
-        {"code_bytes": _read_wasm_bytes(wasm_contract_path)}
-    ],
+    data['app_state']['08-wasm'] = {
+        "contracts": [
+            {"code_bytes": _read_wasm_bytes(wasm_contract_path)}
+        ],
+    }
 
     updated_genesis = json.dumps(data, indent=4)
     with open(genesis_file_path, "w") as outfile:
@@ -32,9 +33,9 @@ def _modify_genesis(genesis_contents, genesis_file_path, wasm_contract_path):
 
 
 def main():
-    file_contents = sys.argv[1]
-    file_path = sys.argv[2]
-    wasm_contract_path = sys.argv[3]
+    file_contents: str = sys.argv[1]
+    file_path: str = sys.argv[2]
+    wasm_contract_path: str = sys.argv[3]
     _modify_genesis(file_contents, file_path, wasm_contract_path)
 
 
